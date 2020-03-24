1ae45b19635eae27b7456639f08101df7a207576
Gino Canessa <ginocanessa@gmail.com>
2020-03-24 17:00:09 -0500

    End of day check-in. Generation of Locations and Groups is almost complete.

---
+++ generator-cli/Geographic/GeoManager.cs
@@ -0,0 +1,97 @@
+﻿// <copyright file="LocationManager.cs" company="Microsoft Corporation">
+//     Copyright (c) Microsoft Corporation. All rights reserved.
+//     Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// </copyright>
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace generator_cli.Geographic
+{
+    /// <summary>Manager for locations.</summary>
+    public abstract class GeoManager
+    {
+        private static List<ZipGeoCode> _locations = null;
+        private static Dictionary<string, List<ZipGeoCode>> _locationsByZip = null;
+        private static Dictionary<string, List<ZipGeoCode>> _locationsByCityState = null;
+
+        private static Random _rand = new Random();
+
+        /// <summary>Initializes this object and loads location data.</summary>
+        public static void Init()
+        {
+            string filename = Path.Combine(Directory.GetCurrentDirectory(), "data", "us-zip-code-latitude-and-longitude.json");
+
+            _locations = JsonConvert.DeserializeObject<List<ZipGeoCode>>(File.ReadAllText(filename));
+
+            _locationsByZip = new Dictionary<string, List<ZipGeoCode>>();
+            _locationsByCityState = new Dictionary<string, List<ZipGeoCode>>();
+            foreach (ZipGeoCode loc in _locations)
+            {
+                if (!_locationsByZip.ContainsKey(loc.fields.zip))
+                {
+                    _locationsByZip.Add(loc.fields.zip, new List<ZipGeoCode>());
+                }
+
+                _locationsByZip[loc.fields.zip].Add(loc);
+
+                string cityState = $"{loc.fields.city},{loc.fields.state}".ToUpperInvariant();
+
+                if (!_locationsByCityState.ContainsKey(cityState))
+                {
+                    _locationsByCityState.Add(cityState, new List<ZipGeoCode>());
+                }
+
+                _locationsByCityState[cityState].Add(loc);
+            }
+
+            Console.WriteLine(
+                $"Loaded {_locations.Count} location records" +
+                $", {_locationsByCityState.Count} city/states" +
+                $", {_locationsByZip.Count} zip codes");
+        }
+
+        /// <summary>Address for postal code.</summary>
+        /// <exception cref="ArgumentNullException">      Thrown when one or more required arguments are
+        ///  null.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when one or more arguments are outside the
+        ///  required range.</exception>
+        /// <param name="postalCode">   The postal code.</param>
+        /// <param name="fillCityState">(Optional) True to fill city state.</param>
+        /// <returns>The Address.</returns>
+        public static Hl7.Fhir.Model.Address AddressForPostalCode(
+            string postalCode,
+            bool fillCityState = true)
+        {
+            if (string.IsNullOrEmpty(postalCode))
+            {
+                throw new ArgumentNullException(nameof(postalCode));
+            }
+
+            if (!_locationsByZip.ContainsKey(postalCode))
+            {
+                throw new ArgumentOutOfRangeException(nameof(postalCode));
+            }
+
+            if (fillCityState)
+            {
+                // note: Random.next max is exclusive
+                int index = _rand.Next(0, _locationsByZip[postalCode].Count);
+
+                return new Hl7.Fhir.Model.Address()
+                {
+                    PostalCode = postalCode,
+                    City = _locationsByZip[postalCode][index].fields.city,
+                    State = _locationsByZip[postalCode][index].fields.state,
+                };
+            }
+
+            return new Hl7.Fhir.Model.Address()
+            {
+                PostalCode = postalCode,
+            };
+        }
+    }
+}
--- generator-cli/Program.cs
+++ generator-cli/Program.cs
@@ -6,44 +6,15 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using generator_cli.Geographic;
+using Hl7.Fhir.Model;
 using Newtonsoft.Json;
-using fhir = generator_cli.fhir;
 
 namespace generator_cli
 {
     /// <summary>A program.</summary>
     public class Program
     {
-        private static readonly fhir.CodeableConcept[] _codingIcu = new fhir.CodeableConcept[1]
-        {
-            new fhir.CodeableConcept()
-            {
-                Coding = new fhir.Coding[1]
-                {
-                    new fhir.Coding()
-                    {
-                        Code = "ICU",
-                        System = "http://terminology.hl7.org/ValueSet/v3-ServiceDeliveryLocationRoleType",
-                    },
-                },
-            },
-        };
-
-        private static readonly fhir.CodeableConcept[] _codingHospital = new fhir.CodeableConcept[1]
-        {
-            new fhir.CodeableConcept()
-            {
-                Coding = new fhir.Coding[1]
-                {
-                    new fhir.Coding()
-                    {
-                        Code = "HOSP",
-                        System = "http://terminology.hl7.org/ValueSet/v3-ServiceDeliveryLocationRoleType",
-                    },
-                },
-            },
-        };
-
         /// <summary>Main entry-point for this application.</summary>
         /// <param name="postalCode">     Postal code to use when updating ERP data.</param>
         /// <param name="outputDirectory">Directory to write JSON files.</param>
@@ -70,46 +41,7 @@ namespace generator_cli
             //    throw new ArgumentException("Either --output-directory or --fhir-server-url must be specified.");
             //}
 
-            LoadLocationData();
-        }
-
-        /// <summary>Loads location data.</summary>
-        public static void LoadLocationData()
-        {
-            string filename = Path.Combine(Directory.GetCurrentDirectory(), "data", "us-zip-code-latitude-and-longitude.json");
-
-            List<ZipGeoCode> zips = JsonConvert.DeserializeObject<List<ZipGeoCode>>(File.ReadAllText(filename));
-
-            Console.WriteLine($"Loaded {zips.Count} location records.");
-
-            foreach (ZipGeoCode loc in zips)
-            {
-                if ((loc.fields.state == "WI") && (loc.fields.city == "Madison"))
-                {
-                    Console.WriteLine($"Found: {loc.datasetid}:{loc.recordid} - {loc.fields.city}, {loc.fields.state} {loc.fields.zip}");
-                }
-            }
-        }
-
-        /// <summary>Generates a location.</summary>
-        /// <param name="occupied">  True if this location is occupied.</param>
-        /// <param name="isIcu">     True if is location is an ICU type, false if not.</param>
-        /// <param name="postalCode">Postal code to use when updating ERP data.</param>
-        /// <returns>The location.</returns>
-        public static string GenerateBedLocation(bool occupied, bool isIcu, string postalCode)
-        {
-            fhir.Location location = new fhir.Location()
-            {
-                OperationalStatus = occupied ? fhir.LocationOperationalStatusValues.O : fhir.LocationOperationalStatusValues.U,
-                Type = isIcu ? _codingIcu : _codingHospital,
-                PhysicalType = new fhir.CodeableConcept() { Coding = new fhir.Coding[1] { fhir.LocationPhysicalTypeValues.bd, }, },
-                Address = new fhir.Address()
-                {
-                    PostalCode = postalCode,
-                },
-            };
-
-            return JsonConvert.SerializeObject(location);
+            GeoManager.Init();
         }
     }
 }
